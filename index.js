f45a39787a9a5518663428ce61be2b3a196dd841
Klaudia Sagan <klaudiasagan.code@gmail.com>
2022-12-22 18:34:12 +0100

    cells with data displayed

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const tableUsers = document.querySelector('table');
+const tableBody = document.querySelector('tbody');
 function getUsers(){
     fetch('https://randomuser.me/api/?results=50&inc=name,gender,nat')
       .then(async (response)=>{
         if(response.ok){
-            const data = await response.json()
-            console.log(data.results)
+            const data = await response.json();
+            console.log(data.results);
             data.results.forEach((user) => {
-                //stworzyć rzędy, a w nich komórki z textContent
+                console.log(user)
+                const row = document.createElement('tr');
+                const cellSurname = document.createElement('td');
+                const cellName = document.createElement('td');
+                const cellGender = document.createElement('td');
+                const cellNat = document.createElement('td');
+                cellSurname.textContent = user.name.last;
+                cellName.textContent = user.name.first;
+                cellGender.textContent = user.gender;
+                cellNat.textContent = user.nat;
+                row.append(cellSurname);
+                row.append(cellName);
+                row.append(cellGender);
+                row.append(cellNat);
+                tableBody.append(row);
             });
+            tableUsers.append(tableBody);
         }else{
             throw new Error('Wystąpił błąd spróbuj ponownie później')
         }
@@ -18,8 +35,8 @@ getUsers();
 
 
 // <tr>
-//<th>Katarzyna</th> 
-//<td>Stosio</td> 
+//<th>Stosio</th> 
+//<td>Katarzyna</td> 
 //<td>Kobieta</td> 
 //<td>Polska</td> 
 //<td>Średnie</td> 
